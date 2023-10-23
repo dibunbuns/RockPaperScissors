31531fc8d17b4d0130eb889a920f4fef91cf4dc0
dibunbuns <bunniequeenrules@gmail.com>
2023-10-23 15:23:06 -0400

    alright lets try this now - DOMContentLoaded

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-let playerScore = 0;
-let computerScore = 0;
+window.addEventListener("DOMContentLoaded", (e) => {
+  let playerScore = 0;
+  let computerScore = 0;
 
-function getComputerChoice() {
-  let options = ["Rock", "Paper", "Scissors"];
-  return options[Math.floor(Math.random() * options.length)];
-}
+  function getComputerChoice() {
+    let options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+  }
 
-function game() {
-  while (playerScore < 5 && computerScore < 5) {
-    let playerSelect = prompt("Choose: ROCK, PAPER, or SCISSORS");
-    if (playerSelect === null) {
-      alert("Hey don't leave me :(");
-      return;
+  function game() {
+    while (playerScore < 5 && computerScore < 5) {
+      let playerSelect = prompt("Choose: ROCK, PAPER, or SCISSORS");
+      if (!playerSelect) {
+        alert("Hey don't leave me :(");
+        return;
+      }
+      let playerSelection =
+        playerSelect.charAt(0).toUpperCase() +
+        playerSelect.slice(1).toLowerCase();
+      if (
+        playerSelection !== "Rock" &&
+        playerSelection !== "Paper" &&
+        playerSelection !== "Scissors"
+      ) {
+        alert("nope, try again.");
+        continue;
+      }
+      const computerSelection = getComputerChoice();
+      alert(playRound(playerSelection, computerSelection));
     }
-    let playerSelection =
-      playerSelect.charAt(0).toUpperCase() +
-      playerSelect.slice(1).toLowerCase();
-    if (
-      playerSelection !== "Rock" &&
-      playerSelection !== "Paper" &&
-      playerSelection !== "Scissors"
+    if (playerScore === 5) {
+      alert("You won the game!");
+    } else if (computerScore === 5) {
+      alert("You lost the game. Better luck next time.");
+    }
+  }
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return `It's a tie! Score: ${playerScore}:${computerScore}`;
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper")
     ) {
-      alert("nope, try again.");
-      continue;
+      playerScore += 1;
+      return `You win this round! ${playerSelection} beats ${computerSelection}! Score: ${playerScore}:${computerScore}`;
+    } else {
+      computerScore += 1;
+      return `You lost this round! ${computerSelection} beats ${playerSelection}! Score: ${playerScore}:${computerScore}`;
     }
-    const computerSelection = getComputerChoice();
-    alert(playRound(playerSelection, computerSelection));
   }
-  if (playerScore === 5) {
-    alert("You won the game!");
-  } else if (computerScore === 5) {
-    alert("You lost the game. Better luck next time.");
-  }
-}
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return `It's a tie! Score: ${playerScore}:${computerScore}`;
-  } else if (
-    (playerSelection === "Rock" && computerSelection === "Scissors") ||
-    (playerSelection === "Paper" && computerSelection === "Rock") ||
-    (playerSelection === "Scissors" && computerSelection === "Paper")
-  ) {
-    playerScore += 1;
-    return `You win this round! ${playerSelection} beats ${computerSelection}! Score: ${playerScore}:${computerScore}`;
-  } else {
-    computerScore += 1;
-    return `You lost this round! ${computerSelection} beats ${playerSelection}! Score: ${playerScore}:${computerScore}`;
-  }
-}
-game();
+  game();
+});
 
 //to test:
 // let result = getComputerChoice();
